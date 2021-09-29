e2be77e8843eaee01784f5ddce4f37937adfeb02
Jackob <pietrzyk.jakub001@gmail.com>
2021-09-29 16:31:44 +0200

    Changed a bit front end

--- src/main/java/com/trix/uploader/config/SpringSecurityConfig.java
+++ src/main/java/com/trix/uploader/config/SpringSecurityConfig.java
@@ -3,16 +3,24 @@ package com.trix.uploader.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.authorizeRequests().anyRequest().permitAll();
-       http.requiresChannel().anyRequest().requiresSecure();
+        http.authorizeRequests().anyRequest().permitAll();
+        http.requiresChannel().anyRequest().requiresSecure();
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
+    }
 
 
 }
