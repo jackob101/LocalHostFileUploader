5650b07659323ca976299587c68c14589d94729a
Jackob <pietrzyk.jakub001@gmail.com>
2021-09-29 12:09:32 +0200

    Application now read config file outside of jar

--- src/main/java/com/trix/uploader/UploaderApplication.java
+++ src/main/java/com/trix/uploader/UploaderApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UploaderApplication {
 
+    public static String configFileName = "config.properties";
+
     public static void main(String[] args) {
 
         boolean runConfig = false;
---
+++ src/main/java/com/trix/uploader/config/ApplicationConfigFile.java
@@ -0,0 +1,21 @@
+package com.trix.uploader.config;
+
+import com.trix.uploader.UploaderApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class ApplicationConfigFile {
+
+    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+
+        PropertySourcesPlaceholderConfigurer properties = new PropertySourcesPlaceholderConfigurer();
+
+        String jarLocation = System.getProperty("user.dir");
+
+        properties.setLocation(new FileSystemResource(jarLocation + "/" + UploaderApplication.configFileName));
+
+        return properties;
+    }
+}
